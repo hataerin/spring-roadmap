ac59233b19f332dc5ee448cfb46baccf5e4e2e39
hataerin <goil1113@likelion.org>
2023-06-30 16:19:04 +0900

    섹션2: api통신 방식

--- 01-Spring-Introduction/src/main/java/hello/spring/controller/HelloController.java
+++ 01-Spring-Introduction/src/main/java/hello/spring/controller/HelloController.java
@@ -4,19 +4,50 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
 
+    //정적 컨텐츠: 화면 그대로 내려줌
     @GetMapping("hello")
     public String hello(Model model) {
         model.addAttribute("data", "hello!!");
         return "hello";
     }
 
+    //MVC 템플릿엔진 : 변환된 화면 내려줌
     @GetMapping("hello-mvc")
     public String helloMvc(@RequestParam(value = "name") String name, Model model) {
         model.addAttribute("name", name);
         return "hello-template";
+    }
+
+    //API: 단순 string 내려줌
+    @GetMapping("hello-string")
+    @ResponseBody //http에서 body부에 데이터를 직접 넣어주겠다
+    public String helloString(@RequestParam("name") String name) {
+        return "hello" + name;
+    }
+
+    //API:  json 내려줌
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
